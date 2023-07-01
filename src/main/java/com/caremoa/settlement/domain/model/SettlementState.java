c12b7d362f3cc94bdb13c334a4be4fe23f3f3bbc
hyeonjae <alskdiel@gmail.com>
2023-07-01 15:12:13 +0900

    modified kafka consumer group

--- src/main/java/com/caremoa/settlement/domain/model/SettlementState.java
+++ src/main/java/com/caremoa/settlement/domain/model/SettlementState.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum SettlementState {
 
-    CREATED("99", "생"), REQUESTED("00", "요청"), APPROVED("10", "승인"), PAID("20", "지급완료");
+    CREATED("99", "생성"), REQUESTED("00", "요청"), APPROVED("10", "승인"), PAID("20", "지급완료");
 	
 	private final String key;
 	private final String title;
